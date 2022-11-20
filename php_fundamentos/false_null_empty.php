4bab688fc6739b745abef0152d07211e4dcf3cfc
Pedro Henrique <pedrohbc96@gmail.com>
2022-11-20 17:15:40 -0300

    Adiciona diferenças entre false, null e empty

---
+++ php_fundamentos/false_null_empty.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP</title>
+</head>
+
+<body>
+
+    <?php
+    
+        //false(true/false) - tipo variável boolean
+        //null e empty - valores especiais
+
+        $funcionario1 = null;
+        $funcionario2 = "";
+        $funcionario3 = false;
+
+        //valores null
+        if (is_null($funcionario1)) {
+            echo "Sim, a variável é null";
+        } else {
+            echo "Não, a variável não é null";
+        }
+
+        echo "<br />";
+
+        if (is_null($funcionario2)) {
+            echo "Sim, a variável é null";
+        } else {
+            echo "Não, a variável não é null";
+        }
+
+        echo "<br />";
+
+        //valores vazios
+        if (empty($funcionario1)) {
+            echo "Sim, a variável está vazia";
+        } else {
+            echo "Não, a variável não está vazia";
+        }
+
+        echo "<br />";
+
+        if (empty($funcionario2)) {
+            echo "Sim, a variável está vazia";
+        } else {
+            echo "Não, a variável não está vazia";
+        }
+
+        echo "<br />";
+
+        //valores negativos
+        if (is_null($funcionario3)) {
+            echo "Sim, a variável é null";
+        } else {
+            echo "Não, a variável não é null";
+        }
+
+        echo "<br />";
+
+        if (empty($funcionario3)) {
+            echo "Sim, a variável está vazia";
+        } else {
+            echo "Não, a variável não está vazia";
+        }
+
+    ?>
+
+</body>
+
+</html>
