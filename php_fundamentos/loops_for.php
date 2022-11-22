42e172e1bc147f1d2066f2d7ebad4dcc6d02c923
Pedro Henrique <pedrohbc96@gmail.com>
2022-11-22 20:23:32 -0300

    Adiciona loop for

---
+++ php_fundamentos/loops_for.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Curso PHP</title>
+</head>
+<body>
+
+	<?php
+
+        for ($x = 1; $x <= 10; $x++) { 
+            echo "$x <br/>";
+        }
+
+	?>
+
+</body>
+</html>
